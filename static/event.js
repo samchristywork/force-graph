f7825c3c6205c9a7ef302ffbd91fac278552e21f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-16 21:10:57 -0500

    feat: Add event module
    This module includes callbacks for events. Right now there are only
    mouse events and resize events.

---
+++ static/event.js
@@ -0,0 +1,28 @@
+canvas.addEventListener("mousedown", function(_) {
+  frame = 0
+  let body = get_body_under_mouse()
+  if (body) {
+    current_body = body
+  }
+})
+
+canvas.addEventListener("mousemove", function(event) {
+  let rect = canvas.getBoundingClientRect()
+  mouse.x = (event.clientX - rect.left) / canvas.width * 500
+  mouse.y = (event.clientY - rect.top) / canvas.height * 500
+
+  frame = 0
+  if (current_body) {
+    current_body.pos.x = mouse.x
+    current_body.pos.y = mouse.y
+  }
+})
+
+canvas.addEventListener("mouseup", function(_) {
+  current_body = null
+})
+
+window.addEventListener("resize", function() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+})
