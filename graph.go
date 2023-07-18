bd0c4d88f8e434015cb8672a4dc9d27c6a450e6d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-17 21:17:36 -0500

    feat: Add function to visit nodes
    This is a recursive function that will visit nodes and add them to a
    list of nodes and edges.

--- graph.go
+++ graph.go
@@ -18,5 +18,55 @@ type Node struct {
 
 var visited = make(map[string]bool)
 
+func visitNode(path, root string) ([]Node, []Edge) {
+	if _, ok := visited[root]; ok {
+		return nil, nil
+	}
+
+	visited[root] = true
+
+	contents, err := ioutil.ReadFile(path + "/" + root + ".dm")
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil
+	}
+
+	tagMap := make(map[string]bool)
+
+	for i := 0; i < len(contents); i++ {
+		if contents[i] == '[' {
+			for j := i; j < len(contents); j++ {
+				if contents[j] == '\n' {
+					break
+				}
+				if contents[j] == ']' {
+					tagname := string(contents[i+1 : j])
+
+					if _, ok := tagMap[tagname]; !ok {
+						tagMap[tagname] = true
+					}
+
+					break
+				}
+			}
+		}
+	}
+
+	nodes := make([]Node, 0)
+	edges := make([]Edge, 0)
+
+	nodes = append(nodes, Node{name: root, color: "red"})
+
+	for tagname := range tagMap {
+		new_nodes, new_edges := visitNode(path, tagname)
+		nodes = append(nodes, new_nodes...)
+		edges = append(edges, new_edges...)
+
+		edges = append(edges, Edge{from: root, to: tagname})
+	}
+
+	return nodes, edges
+}
+
 func process_files(path string, root string) {
 }
