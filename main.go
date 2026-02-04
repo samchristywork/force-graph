65392bc99ac94821c3a5016aaad007073c4dc5bc
samchristywork <106397573+samchristywork@users.noreply.github.com>
2026-02-03 19:36:39 -0600

    feat: Add endpoint to list all possible tags

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
 
 var validTag = regexp.MustCompile(`^[a-zA-Z0-9_-]+$`)
@@ -38,12 +40,29 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(str))
 }
 
+func tagsHandler(w http.ResponseWriter, r *http.Request) {
+	entries, err := os.ReadDir("./testData")
+	if err != nil {
+		http.Error(w, `{"error":"could not read tags"}`, http.StatusInternalServerError)
+		return
+	}
+	tags := make([]string, 0, len(entries))
+	for _, e := range entries {
+		if !e.IsDir() && strings.HasSuffix(e.Name(), ".dm") {
+			tags = append(tags, strings.TrimSuffix(e.Name(), ".dm"))
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tags)
+}
+
 func serve() {
 	dirname := "./static"
 	fs := http.FileServer(http.Dir(dirname))
 
 	http.Handle("/", fs)
 	http.Handle("/data.json", http.HandlerFunc(dataHandler))
+	http.Handle("/tags", http.HandlerFunc(tagsHandler))
 
 	fmt.Printf("Serving %s on HTTP port: 8080\n", dirname)
 	log.Fatal(http.ListenAndServe(":8080", nil))
